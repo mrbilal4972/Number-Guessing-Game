8cd6dd89dfd7240e513754d719a9e78dd5e46262
mrbilal4972 <mrbilal4972@users.noreply.github.com>
2021-02-25 00:17:58 +0500

    Try/Except is implemented for invalid input exception

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ totalGuesses = 0
 
 # Loop runs until guess matched
 while randNo!=userGuess:
-    userGuess = int(input("Guess the number between 1 to 100: "))
-    if randNo != userGuess:
-        if userGuess>randNo:
-            print("Your guess is wrong, Select the smaller number")
+    try: # Try box handle the invalid input from user, like: alphbet or special character
+        userGuess = int(input("Guess the number between 1 to 100: "))
+        if randNo != userGuess:
+            if userGuess>randNo:
+                print("Your guess is wrong, Select the smaller number")
+            else:
+                print("Your guess is wrong, Select the Larger number")
         else:
-            print("Your guess is wrong, Select the Larger number")
-    else:
-        print("Congratulations!! Your guess is correct")
-    totalGuesses += 1
+            print("Congratulations!! Your guess is correct")
+        totalGuesses += 1
+    except Exception as e:
+        print("Invalid Input!! Please Enter Valid Number")
 
 print(f'''you got the correct answer after "{totalGuesses}" Guesses''')
 
